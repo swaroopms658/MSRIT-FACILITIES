929114304c5d8eda781f3c174b4158dc9ed096f7
C S Darshan <96053838+C-S-Darshan@users.noreply.github.com>
2025-06-03 06:01:55 +0000

    changed directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -170,7 +170,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/booking" element={<Booking />} />
